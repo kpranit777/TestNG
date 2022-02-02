b7b23107dc607908afb3180a27e2047edaa834c7
kpranit777 <97101413+kpranit777@users.noreply.github.com>
2022-02-02 15:10:37 +0530

    Add files via upload

---
+++ src/test/java/com/mycompany/app/Parallel_Test_Thread3.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class Parallel_Test_Thread3 {
+  @Test
+  public void f3() {
+	  System.out.println(Thread.currentThread().getId());
+  }
+}
---
+++ src/test/java/com/mycompany/app/Parallel_Test_Thread6.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class Parallel_Test_Thread6 {
+  @Test
+  public void f6() {
+	  System.out.println(Thread.currentThread().getId());
+  }
+}
